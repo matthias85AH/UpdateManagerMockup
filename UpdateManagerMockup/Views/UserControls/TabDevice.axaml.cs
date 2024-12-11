170d719222488082337f91a9db54f24dd6f347a8
matthias.thomi@adamhall.com <matthias.thomi@adamhall.com>
2024-12-11 10:25:30 +0100

    Added user controls together with TabControl

---
+++ UpdateManagerMockup/Views/UserControls/TabDevice.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UpdateManagerMockup.Views.UserControls;
+
+public partial class TabDevice : UserControl
+{
+    public TabDevice()
+    {
+        InitializeComponent();
+    }
+}
---
+++ UpdateManagerMockup/Views/UserControls/TabInterface.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace UpdateManagerMockup.Views.UserControls;
+
+public partial class TabInterface : UserControl
+{
+    public TabInterface()
+    {
+        InitializeComponent();
+    }
+}
