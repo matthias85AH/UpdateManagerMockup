baa60106c5b320c82fc06c8b06c899ef3d953038
matthias.thomi@adamhall.com <matthias.thomi@adamhall.com>
2024-12-11 09:12:53 +0100

    Initial Commit

---
+++ UpdateManagerMockup/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace UpdateManagerMockup.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ UpdateManagerMockup/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace UpdateManagerMockup.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
