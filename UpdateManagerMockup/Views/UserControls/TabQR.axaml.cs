790cf212ee5e7192ba1a6b19e36dc2629fd8f5d1
matthias.thomi@adamhall.com <matthias.thomi@adamhall.com>
2025-01-07 14:20:08 +0100

    QR Code working on android

--- UpdateManagerMockup/Views/UserControls/TabQR.axaml.cs
+++ UpdateManagerMockup/Views/UserControls/TabQR.axaml.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -8,6 +11,10 @@ using Avalonia.Platform;
 using Avalonia.Threading;
 using CommunityToolkit.Maui.Views;
 using Microsoft.Maui;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using ZXing;
+
 
 namespace UpdateManagerMockup.Views.UserControls;
 
@@ -54,10 +61,75 @@ public partial class TabQR : UserControl
 
     private void CamView_MediaCaptured(object? sender, MediaCapturedEventArgs e)
     {
+        Bitmap bitmapImg = new Bitmap(e.Media);
+
         Dispatcher.UIThread.Post(() => 
         {
-            imgPicture.Source = new Bitmap(e.Media);
+            imgPicture.Source = bitmapImg;
         });
+
+        e.Media.Seek(0, SeekOrigin.Begin);
+
+        //byte[] imgJpegData;
+        //using (MemoryStream ms = new MemoryStream())
+        //{
+        //    e.Media.CopyTo(ms);  // Copies all bytes from the stream to memoryStream
+        //    imgJpegData = ms.ToArray();  // Returns the byte array from the memory stream
+        //}
+
+        //byte[] imgRawData;
+        //using (MemoryStream ms = new MemoryStream())
+        //{
+        //    bitmapImg.Save(ms);
+        //    imgRawData = ms.ToArray();  // Returns the byte array from the memory stream
+        //}
+
+        //Dispatcher.UIThread.Post(() =>
+        //{
+        //    txtDetectedContent.Text = "";
+        //    txtDetectedContent.Text += $"ImageData Size: {bitmapImg.Size.Width} x {bitmapImg.Size.Height}{Environment.NewLine}";
+        //    txtDetectedContent.Text += $"ImageData Format: {bitmapImg.Format}{Environment.NewLine}";
+        //    txtDetectedContent.Text += $"ImageData Length: {imgJpegData.Length}{Environment.NewLine}";
+        //    txtDetectedContent.Text += $"First Bytes: {string.Join(" ", imgJpegData.Take(10))}{Environment.NewLine}";
+        //    txtDetectedContent.Text += $"Raw Data Length: {imgRawData.Length}{Environment.NewLine}";
+        //    txtDetectedContent.Text += $"First Bytes: {string.Join(" ", imgRawData.Take(10))}{Environment.NewLine}";
+        //});
+
+        var imageSharpImg = SixLabors.ImageSharp.Image.Load<Rgb24>(e.Media);
+
+        var reader = new ZXing.ImageSharp.BarcodeReader<Rgb24>();
+
+        reader.Options.PossibleFormats = new List<BarcodeFormat>() { BarcodeFormat.QR_CODE, BarcodeFormat.DATA_MATRIX, BarcodeFormat.AZTEC };
+
+        Result? decodeResult = null;
+
+        try
+        {
+            decodeResult = reader.Decode<Rgb24>(imageSharpImg);
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+
+        if (decodeResult != null)
+        {
+            Dispatcher.UIThread.Post(() =>
+            {
+                txtDetectedContent.Text = "";
+                txtDetectedContent.Text += $"Decode Result{Environment.NewLine}";
+                txtDetectedContent.Text += $"Barcode Format: {decodeResult.BarcodeFormat}{Environment.NewLine}";
+                txtDetectedContent.Text += $"Text: {decodeResult.Text}{Environment.NewLine}";
+            });
+        }
+        else
+        {
+            Dispatcher.UIThread.Post(() =>
+            {
+                txtDetectedContent.Text = "";
+                txtDetectedContent.Text += $"No Barcode found {DateTime.Now.Second}";
+            });
+        }
     }
 
     private void TakePicture_Click(object sender, RoutedEventArgs e)
